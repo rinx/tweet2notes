d4fca963155a693694326395204f2a88f968886e
Rintaro Okamura <rintaro.okamura@gmail.com>
2013-02-03 11:22:25 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweet2notes::Application.config.secret_token = '8dd82214dee6e4d2ef55ef55a0e71999f28362bd5f0251e0b7b6f7536bd0922581c42e4e6a5cf1db113873444c13674910828841def45781827b25b9117b6853'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tweet2notes::Application.config.session_store :cookie_store, key: '_tweet2notes_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tweet2notes::Application.config.session_store :active_record_store
