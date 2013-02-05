342d2e8d784b990b946fa5b22b1e1547fe614f08
Rintaro Okamura <rintaro.okamura@gmail.com>
2013-02-05 09:57:00 +0900

    login_controller.rb is written

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,13 +1,27 @@
 class LoginController < ApplicationController
   def twCallback
+    session[:twtoken] = request.env['omniauth.auth']['credentials']['token']
+    session[:twsecret] = request.env['omniauth.auth']['credentials']['secret']
+
+    redirect_to '/'
   end
 
   def enCallback
+    session[:entoken] = request.env['omniauth.auth']['credentials']['token']
+
+    redirect_to '/'
   end
 
   def twLogout
+    session[:twtoken] = nil
+    session[:twsecret] = nil
+
+    redirect_to '/'
   end
 
   def enLogout
+    session[:entoken] = nil
+
+    redirect_to '/'
   end
 end
