95c9c2cf4df4343edeb129a5ffc44a5e2f8fdc0f
rinx <rintaro.okamura@gmail.com>
2013-02-12 21:19:36 +0900

    parameter/input redirect process is written

--- app/controllers/parameter_controller.rb
+++ app/controllers/parameter_controller.rb
@@ -1,5 +1,14 @@
 class ParameterController < ApplicationController
   def input
+
+    if (session[:twtoken] == nil) or 
+       (session[:twsecret] == nil) or
+       (session[:entoken] == nil) then
+
+      redirect_to root_url, :notice => "please login both!!"
+
+    end
+
   end
 
   def register
